b70c62c57d89b78e2e5097f8477cc2c6a42522cb
guilhermesouza23 <144975743+guilhermesouza23@users.noreply.github.com>
2023-11-22 20:59:04 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,24 @@
+function submitForm() {
+    // Obter valores dos campos
+    var nome_motorista = document.getElementById('nome_motorista').value;
+    var cpf = document.getElementById('cpf').value;
+    var rg = document.getElementById('rg').value;
+    var telefone = document.getElementById('telefone').value;
+    var transportadora = document.getElementById('transportadora').value;
+    var tipo_veiculo = document.getElementById('tipo_veiculo').value;
+    var tipo_veiculo_carga = document.getElementById('tipo_veiculo_carga').value;
+    var placa = document.getElementById('placa').value;
+    var placa_carreta = document.getElementById('placa_carreta').value;
+
+    // Aqui você pode fazer o que quiser com os dados, por exemplo, enviá-los para um servidor
+    // Neste exemplo, apenas exibimos os dados no console
+    console.log("Nome do Motorista: " + nome_motorista);
+    console.log("CPF: " + cpf);
+    console.log("RG: " + rg);
+    console.log("Telefone: " + telefone);
+    console.log("Transportadora: " + transportadora);
+    console.log("Tipo de Veículo: " + tipo_veiculo);
+    console.log("Tipo de Veículo de Carga: " + tipo_veiculo_carga);
+    console.log("Placa: " + placa);
+    console.log("Placa da Carreta: " + placa_carreta);
+}
