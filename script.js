06ce703eef15eef3c9375c83b520588fd5bad546
guilhermesouza23 <144975743+guilhermesouza23@users.noreply.github.com>
2023-11-22 21:13:19 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function submitForm() {
     // Obter valores dos campos
-    var nome_motorista = document.getElementById('nome_motorista').value;
     var cpf = document.getElementById('cpf').value;
+    var nome_motorista = document.getElementById('nome_motorista').value;
     var rg = document.getElementById('rg').value;
     var telefone = document.getElementById('telefone').value;
     var transportadora = document.getElementById('transportadora').value;
@@ -22,3 +22,26 @@ function submitForm() {
     console.log("Placa: " + placa);
     console.log("Placa da Carreta: " + placa_carreta);
 }
+
+// Função para formatar o horário atual
+function formatarHorario() {
+    var dataAtual = new Date();
+    var horas = dataAtual.getHours();
+    var minutos = dataAtual.getMinutes();
+    minutos = minutos < 10 ? '0' + minutos : minutos; // Adiciona um zero à esquerda se for menor que 10
+    var horarioFormatado = horas + ':' + minutos;
+    return horarioFormatado;
+}
+
+// Função para ser chamada quando a página carregar
+window.onload = function () {
+    // Configurar a data atual no campo de data
+    var campoData = document.getElementById('data');
+    var dataAtual = new Date();
+    var dataFormatada = dataAtual.toISOString().split('T')[0];
+    campoData.value = dataFormatada;
+
+    // Configurar o horário atual no campo de horário
+    var campoHorario = document.getElementById('horario');
+    campoHorario.value = formatarHorario();
+};
